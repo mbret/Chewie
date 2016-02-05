5563936fd62af7909f09b85cd9024c006dbf1d96
Maxime Bret <maxime.bret.ext@boursorama.fr>
2016-02-05 18:46:20 +0100

    no message

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 'use strict';
 
-var MyBuddySystem = require('my-buddy-system');
+/**
+ *
+ * Note that the system may restart automatically
+ * .start() is protected against worker cluster but all the code
+ * written here will be called twice.
+ */
+
+var cluster         = require('cluster');
+var config          = require('./config.js');
+var MyBuddySystem   = require('my-buddy-system');
+
+// Use your own custom plugins repository
+MyBuddySystem.registerNewPluginDirectory(__dirname + '/_dev_plugins');
+
+// Use your own config
+MyBuddySystem.registerNewConfig(config);
+
+// Start the system
+// You don't need anything else after this point.
+// The system handle itself completely.
+MyBuddySystem.start();
