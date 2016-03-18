875bd6c5b749e1e69133b25df315647ce64cc9ab
Maxime Bret <maxime.bret.ext@boursorama.fr>
2016-03-18 15:31:05 +0100

    no message

--- index.js
+++ index.js
@@ -7,14 +7,7 @@
  * written here will be called twice.
  */
 
-var config          = require('./config.js');
 var MyBuddySystem   = require('my-buddy-system');
-
-// Use your own custom plugins repository
-MyBuddySystem.registerNewPluginDirectory('./plugins');
-
-// Use your own config
-MyBuddySystem.registerNewConfig(config);
 
 // Start the system
 // You don't need anything else after this point.
